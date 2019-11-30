bec4cc6dca41a04dd150d07d377fd9aef18eb68d
Tiago Valdrich <tiago.valdrich@camerite.com>
2019-11-30 20:05:09 -0300

    Making validations and improvements on code.

--- exercise_1.py
+++ exercise_1.py
@@ -1,14 +1,30 @@
+import re
+
 def main():
     SLOWDOWN = -5
 
-    print('Insert the initial speed: ')
-    initial_speed = float(input())
+    try:
+        while True:
+            print('Insert the initial speed: ')
+            initial_speed = input()
+
+            if has_only_numbers(initial_speed):
+                initial_speed = float(initial_speed)
+
+            if (type(initial_speed) is float) and (initial_speed > 0):
+                break
+    except:
+        print('Invalid value inserted. Please, insert only numbers.')
+        exit()
 
     time = calc_slowdown_time(0, initial_speed, SLOWDOWN)
     distance = calc_distance(0, initial_speed, time, SLOWDOWN)
 
-    print('Time spent ' + str(time))
-    print('Distance ' + str(distance))
+    print('\nTime spent to stop the car: %.2f seconds' % time)
+    print('Travelled distance: %.2f meters' % distance)
+
+def has_only_numbers(input_string):
+    return bool(re.search(r'-?\d+', input_string))
 
 
 def calc_slowdown_time(final_speed, initial_speed, acceleration):
@@ -16,7 +32,7 @@ def calc_slowdown_time(final_speed, initial_speed, acceleration):
 
 
 def calc_distance(initial_distance, initial_speed, time, acceleration):
-    return round((initial_distance + (initial_speed * time) + ((acceleration * pow(time, 2)) / 2)))
+    return ((initial_distance + (initial_speed * time) + ((acceleration * pow(time, 2)) / 2)))
 
 
 if __name__ == '__main__':
