0420a3e9ebaf70dc4e70694b2b0c3fb9233cca33
LuísFZanatta <luiszanuto@hotmail.com>
2019-12-02 00:51:42 -0300

    Allan Topper 2.1

--- exercise_6.py
+++ exercise_6.py
@@ -1,18 +1,19 @@
+import re
+
 def main():
-    print('Insert a number of wagons')
-    numb = int(input())
+
+    numb = ask_for_int_number_on_input('Insert a number of wagons')
 
     wags = []
     total_wd = 0
 
     for i in range( 0 , numb ):
-        print('Insert the weight of the wagon ' + str(i+1) + ':' )
-        wags.insert(i , float(input()))
+        wags.insert(i , ask_for_float_number_on_input('Insert the weight of the wagon ' + str(i+1) + ':' ) )
         total_wd += wags[i]
 
 
-    print('Insert a force for locomotive')
-    force = float(input())
+
+    force =  ask_for_float_number_on_input('Insert a force for locomotive')
 
     acel =  calc_acel( force , total_wd )
 
@@ -32,6 +33,52 @@ def main():
 
         # Wag Tension
         print('Wagon tension ' + str(i+1) + ' : ' + str(f) )
+
+
+
+def has_only_numbers(input_string):
+    return bool(re.search(r'-?\d+', input_string))
+
+
+def ask_for_float_number_on_input(message):
+    try:
+        value = 0.0
+
+        while True:
+            print(message)
+            value = input()
+
+            if has_only_numbers(value):
+                value = float(value)
+
+            if (type(value) is float) and (value > 0):
+                break
+
+        return value
+    except:
+        print('Invalid value inserted. Please, insert only numbers.')
+        exit()
+
+
+def ask_for_int_number_on_input(message):
+    try:
+        value1 = 0
+
+        while True:
+            print(message)
+            value1 = input()
+
+            if has_only_numbers(value1):
+                value1 = int(value1)
+
+            if (type(value1) is int) and (value1 > 0):
+                break
+
+        return value1
+    except:
+        print('Invalid value inserted. Please, insert only numbers.')
+        exit()
+
 
 
 def calc_acel( F , m ):
